9aa1725fcfa587b631eaa2abc16ed3687830bd8c
Beonikol <68246520+Beonikol@users.noreply.github.com>
2025-11-20 12:06:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import md from 'vite-plugin-md'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
+  plugins: [react(), md()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.md'],
+  },
 })
